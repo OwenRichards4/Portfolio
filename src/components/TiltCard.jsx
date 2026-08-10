1128cb286b3b2011617e9d6e8b0582675d7f9fda
Owen Richards <owencrichards@gmail.com>
2026-08-10 18:21:56 -0400

    Bump tilt card max rotation from 2deg to 3deg

--- src/components/TiltCard.jsx
+++ src/components/TiltCard.jsx
@@ -9,8 +9,8 @@ export default function TiltCard({ children, className = "" }) {
   const x = useMotionValue(0.5);
   const y = useMotionValue(0.5);
 
-  const rotateX = useSpring(useTransform(y, [0, 1], [2, -2]), SPRING);
-  const rotateY = useSpring(useTransform(x, [0, 1], [-2, 2]), SPRING);
+  const rotateX = useSpring(useTransform(y, [0, 1], [3, -3]), SPRING);
+  const rotateY = useSpring(useTransform(x, [0, 1], [-3, 3]), SPRING);
   const glowX = useTransform(x, [0, 1], ["0%", "100%"]);
   const glowY = useTransform(y, [0, 1], ["0%", "100%"]);
   const glowBackground = useTransform(
